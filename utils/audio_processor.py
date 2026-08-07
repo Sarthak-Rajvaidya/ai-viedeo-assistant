ba7e0da962d6759ee203b7dffda30040f4b6daf8
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-08-07 15:16:30 +0530

    feat(audio): add audio conversion and chunking for Whisper preprocessing

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -24,4 +24,35 @@ def download_youtube_audio(url :str) ->str:
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
-print(download_youtube_audio("https://www.youtube.com/watch?v=7HSSR1n8dgc"))
+data = download_youtube_audio("https://www.youtube.com/watch?v=8u4r__aPzRI")
+
+
+
+def convert_to_wav(input_path: str) -> str:
+    """Convert any audio/video file to WAV format using pydub."""
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000) #16khz
+    audio.export(output_path, format="wav")
+    return output_path
+data_final = (convert_to_wav(data))
+
+def chunk_audio(wav_path :str,chunk_minutes:int=10)-> list:
+    audio = AudioSegment.from_wav(wav_path)
+    
+    chunk_ms = chunk_minutes*60*1000 # minutes tos econd to millisecond
+    
+    
+    chunks = []
+    
+    for i,start in enumerate(range(0,len(audio),chunk_ms)):
+        chunk = audio[start : start+chunk_ms]
+        
+        chunk_path = f"{wav_path}_chunk{i}.wav"
+        
+        chunk.export(chunk_path,format = "wav")
+        
+        chunks.append(chunk_path)
+    return chunks
+
+print(chunk_audio(data_final))
