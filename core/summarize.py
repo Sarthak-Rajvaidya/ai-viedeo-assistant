31e93682f1b336eb634b7b140bfee96b6b196815
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-08-12 08:18:01 +0530

    feat: Created new file for llm integration and api calling and import libraries

---
+++ core/summarize.py
@@ -0,0 +1,7 @@
+from langchain_mistralai import ChatMistralAI
+
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
