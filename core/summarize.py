0db39ed792b7eeaef255e753e412bc49bf37add8
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-08-13 21:00:03 +0530

    feat : Updated the summarizer.py code added runnables as well as split the text in chunks

--- core/summarize.py
+++ core/summarize.py
@@ -5,3 +5,34 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+from langchain_core.runnables import RunnablePassthrough,RunnableLambda
+
+import os
+
+def get_llm():
+    return ChatMistralAI(model = "mistral-small-latest",mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature = 0.3) #temp for creative response
+
+
+
+def split_transcript(transcript : str)-> list:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 3000,
+        chunk_overlap = 200
+        
+    )
+    
+    return splitter.split_text(transcript)
+
+def summarize(transcript : str) -> str:
+    llm = get_llm()
+    
+    map_prompt = ChatPromptTemplate.from messages(
+        [
+            ("system","Summarize this portion of a meeting transcript concisely."),
+            ("human","{text}"),
+        ]
+        
+        
+    )
